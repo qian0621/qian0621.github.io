2f6e57a7aebae1ca739bab6fc81bea18a72a102d
Ker Qian Ning <kerqianning2005@gmail.com>
2023-09-29 01:24:27 +0800

    Update intercept_contact_form_submission.js

--- intercept_contact_form_submission.js
+++ intercept_contact_form_submission.js
@@ -1,6 +1,8 @@
 document.getElementById('contact_form').addEventListener('submit', function(event) {
     // Prevent the default form submission behavior
     event.preventDefault();
+    // Create a new FormData object from the form
+    const formData = new FormData(this);
     //stop spaces from being encoded as +
     window.location.href = `${this.action}?subject=${formData.get('subject')}&body=${formData.get('body')}`;
 });
